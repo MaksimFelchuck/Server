6de2d96e97a0e6af442fe23825b5661d352d60be
Макс <felchuck@yandex.ru>
2018-07-11 20:35:23 +0700

    Добавление списка скриптов на главную страницу

--- Server/my_app/views.py
+++ Server/my_app/views.py
@@ -1,15 +1,31 @@
 
+
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .forms import *
+from .models import script
 
 # Create your views here.
 from django.views.generic.base import View
 
-def Index(request):
-    return render(request, 'home.html')
+
 def Main(request):
-    return render(request, 'Main.html')
+    records = script.objects.all()
+
+    context = {
+        'dict': records
+    }
+    return render(request, 'Main.html',context)
+
+def Index(request):
+    records = script.objects.all()
+
+    context = {
+        'dict': records
+    }
+    return render(request, 'home.html',context)
+
+
 
 def Script(request):
         form = ScriptForm(request.POST or  None)
